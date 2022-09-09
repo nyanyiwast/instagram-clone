b5dc721e6cfbe20cf47182b88f6f036691eac7ab
nyanyiwast <nyanyiwast@gmail.com>
2022-09-09 20:04:06 +0200

    added 4amTribe logo and created footer bar

---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import { Navbar, Nav, Container } from "react-bootstrap";
+import logo from "../assets/4amtribe Logo - Original - 5000x5000.png"
+
+const Footer = () => {
+     return(
+        <Navbar bg="light" variant="light" fixed="bottom">
+        <Container>
+          <Navbar.Brand>
+            <img src={logo} alt="4amTribeLogo" className="logo" />
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+     )
+};
+
+export default Footer;
