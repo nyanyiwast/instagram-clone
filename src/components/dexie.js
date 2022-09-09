c9cc0d4ed864cedccfa6a9eb1b84c44b6f1a3d16
nyanyiwast <nyanyiwast@gmail.com>
2022-09-09 19:24:17 +0200

    exported dexie function for use outside

--- src/components/dexie.js
+++ src/components/dexie.js
@@ -1,7 +1,7 @@
 import Dexie from "dexie";
 import { version } from "react";
 
-const db = new Dexie("insta-clone")
+export const db = new Dexie("insta-clone")
 db.version(1).stores({
     bio: ",username, about",
     gallery: "++id, imgSrc"
